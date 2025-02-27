0fb7b90b248c5ac7adb1a5933bee7b8370a9d5ad
LucasJinhyeokYang <2021004935@student.sit.ac.nz>
2025-02-27 15:17:40 +1300

    z

---
+++ MVCRevision/Models/Post.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+    public class Post{
+        [Key]
+        public int PostId { get; set; }
+        public required string  Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime? PublishedDate { get; set; }
+    }   
+
